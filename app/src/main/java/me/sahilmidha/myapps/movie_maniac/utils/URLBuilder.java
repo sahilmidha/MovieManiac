3a18053fa99adc01f1a8c0b83616b16037e42236
sahilmidha <sahil.mh@gmail.com>
2016-05-02 03:26:17 +0530

    updated URLs

--- app/src/main/java/me/sahilmidha/myapps/movie_maniac/utils/URLBuilder.java
+++ app/src/main/java/me/sahilmidha/myapps/movie_maniac/utils/URLBuilder.java
@@ -11,28 +11,29 @@ import me.sahilmidha.myapps.movie_maniac.R;
  */
 public class URLBuilder
 {
-    private static final String BASE_URL = "http://api.themoviedb.org/3";
-    private static final String MOVIES_EXPLORE = "/discover/movie";
-    private static final String MOVIE_DETAILS = "/movie/";
+    private static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
+    private static final String POPULAR = "popular";
+    private static final String TOP_RATED = "top_rated";
 
 
     public static String getMoviesListRequest(String sortBy)
     {
         if(sortBy == null){
-            sortBy = "popularity.desc";
+            sortBy = POPULAR;
         }
-        if(sortBy.equalsIgnoreCase("vote_average.desc"))
+        if(sortBy.equalsIgnoreCase(POPULAR))
         {
-            return String.format("%s%s?sort_by=%s&vote_count.gte=50&api_key=", BASE_URL, MOVIES_EXPLORE, sortBy);
+            return String.format("%s%s?vote_count.gte=300&api_key=", BASE_URL, POPULAR);
         }
-        else {
-            return String.format("%s%s?sort_by=%s&api_key=", BASE_URL, MOVIES_EXPLORE, sortBy);
+        else if(sortBy.equalsIgnoreCase(TOP_RATED)) {
+            return String.format("%s%s?vote_count.gte=300&api_key=", BASE_URL, TOP_RATED);
         }
+        return "";
     }
 
     public static String getMovieDetailsRequest(String movieId)
     {
-        return String.format("%s%s/%s/%s?api_key=", BASE_URL, MOVIE_DETAILS, movieId);
+        return String.format("%s%s?api_key=", BASE_URL, movieId);
     }
 
 }
